3cbf50a6f259c7caa4ab690e9868efb227f6e6ed
Arni-Rie Tamayo <arnirietamayo@gmail.com>
2026-09-23 07:13:29 +0800

    update

---
+++ src/uml/Book.java
@@ -0,0 +1,60 @@
+package uml;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private short year;
+    private boolean isAvailable;
+
+    public Book(String isbn, String title){
+        this.isbn = isbn;
+        this.title = title;
+        this.year = 1975;
+        this.isAvailable = true;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public void borrowBook(){
+        if(!isAvailable){
+            System.out.println("Book not available");
+            return;
+        }
+        isAvailable = false;
+    }
+
+    public void returnBook(){
+        isAvailable = true;
+
+    }
+}
+
+void main(){
+    while(condition_false){
+
+    }
+    for(init, condition_false; va){
+
+    }
+    do{
+
+    }while(condition_false);
+}
